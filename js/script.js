36321b6ddbecebdff53cc515c99a365a2d2cfbff
muqniansyah <muqniansyah@gmail.com>
2023-02-05 11:04:46 +0700

    fix eror navbar dan hapus event klik diJS

--- js/script.js
+++ js/script.js
@@ -1,54 +1,35 @@
-// event pada saat link di klik
-$('.page-scroll').on('click', function (event){
-	
-	// ambil isi href
-	let tujuan = $(this).attr('href');
-	//tangkap elemen yang bersangkutan
-	let elemenTujuan = $(tujuan);
-	//masih eror
-	$('body').animate({
-		scrollTop: elemenTujuan.offset().top-50
-	}, 1250, 'easeInOutExpo');
-
-	event.preventDefault();
-});
-
 //parallax efect
 //untuk parallax efek yang diabout
-$(window).on('load', function(){
-	$('.pKiri').addClass('pMuncul');
-	$('.pKanan').addClass('pMuncul');
+$(window).on("load", function () {
+  $(".pKiri").addClass("pMuncul");
+  $(".pKanan").addClass("pMuncul");
 });
 //akhir efek yang di about
 
-$(window).scroll(function(){
-	var wScroll = $(this).scrollTop();
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
 
-	//kecapatan yang berbeda pada jumbotron(efek parallax yang kedua)
-	$('.jumbotron img').css({
-		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
-	});
+  //kecapatan yang berbeda pada jumbotron(efek parallax yang kedua)
+  $(".jumbotron img").css({
+    transform: "translate(0px, " + wScroll / 4 + "%)",
+  });
 
-	$('.jumbotron h1').css({
-		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
-	});
+  $(".jumbotron h1").css({
+    transform: "translate(0px, " + wScroll / 2 + "%)",
+  });
 
-	$('.jumbotron p').css({
-		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
-	});
-	//akhir efek yang jumbotron
+  $(".jumbotron p").css({
+    transform: "translate(0px, " + wScroll / 1.2 + "%)",
+  });
+  //akhir efek yang jumbotron
 
-	//landing page atau elemen pada portofolio
-	if( wScroll > $('.portofolio').offset().top -250 ) {
-		$('.portofolio .thumbnail').each(function(i){
-			setTimeout(function(){
-				$('.portofolio .thumbnail').eq(i).addClass('muncul');
-			}, 300 * (i+1));
-		});
-	}
-	//akhir efek yang portofolio
-
+  //landing page atau elemen pada portofolio
+  if (wScroll > $(".portofolio").offset().top - 250) {
+    $(".portofolio .thumbnail").each(function (i) {
+      setTimeout(function () {
+        $(".portofolio .thumbnail").eq(i).addClass("muncul");
+      }, 300 * (i + 1));
+    });
+  }
+  //akhir efek yang portofolio
 });
-
-
-
